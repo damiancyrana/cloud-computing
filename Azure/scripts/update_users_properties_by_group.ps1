9661fd50f180afce369b93ba6b47360b943aad6d
Damian Cyrana <damiancyrana@gmail.com>
2021-05-11 20:05:47 +0200

    Added scripts 'update_users_properties_by_group'

---
+++ Azure/scripts/update_users_properties_by_group.ps1
@@ -0,0 +1,14 @@
+﻿# massive update users properties by group in azure
+#by Damian Cyrana
+
+
+Import-Module AzureAD
+Connect-AzureAD
+
+$ADUserGroupID = "write Azure AD Group ID here"
+$usersInADGroup = Get-AzureADGroupMember -ObjectID $ADUserGroupID
+
+foreach($user in $usersInADGroup){
+    Write-Host $user.DisplayName
+    Set-AzureADUser -ObjectID $user.Mail -Department "write new value here"
+}
