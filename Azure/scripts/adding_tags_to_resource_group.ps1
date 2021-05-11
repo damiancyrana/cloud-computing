4d69fd6f6fde928c04293089193c78cdcdea5aa9
Damian Cyrana <damiancyrana@gmail.com>
2021-05-12 00:05:38 +0200

    Added script to Azure folder 'adding_tags_to_resource_group'

---
+++ Azure/scripts/adding_tags_to_resource_group.ps1
@@ -0,0 +1,14 @@
+﻿#---- the script will overwrite existing tags ---#
+
+
+Get-AzTag Detailed
+
+(Get-AzResource -TagName department).name
+(Get-AzResource -TagValue ux).name
+
+# Add an existing tag an non existing tag to a resource with a tag already
+$resourceTags = @{'project' = 'ux'; 'location' = 'Warsaw'}
+$resourceGroup = Get-AzResourceGroup -Name 'demo-rg'
+$resourceGroup.ResourceID
+
+New-AzTag = -ResourceID $resourceGroup.ResourceID -Tag $resourceTags
