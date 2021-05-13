344bccacc8dde2f156645a21b2d05314b8f24c7a
Damian Cyrana <damiancyrana@gmail.com>
2021-05-13 13:05:15 +0200

    Added script 'Azure/scripts/create_custom_role.ps1'

---
+++ Azure/scripts/create_custom_role.ps1
@@ -0,0 +1,20 @@
+﻿# Create Azure Custom Role
+# by Damian Cyrana
+
+
+# Role description and basic parameters
+$azureRole = Get-AzRoleDefinition "Write role name"
+$azureRole.ID = $null
+$azureRole.Name = "Role name e.g Network Admin"
+$azureRole.Description = "role description "
+$azureRole.Actions.Clear()
+
+# Specify the resources you want the user to access
+$azureRole.Actions.Add("Microsoft.Storage/*/read")
+$azureRole.Actions.Add("Microsoft.Compute/*/write")
+$azureRole.AssignableScopes.clear()
+
+# Enter the license to which you want to assign the role
+$azureRole.AssignableScopes.Add("/subscriptions/xxxxxx")
+
+New-AzRoleDefinition -Role $azureRole
