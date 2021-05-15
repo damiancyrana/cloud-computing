cc69ba0f17fa05e52efe269fdbffeaa6efb8fd96
Damian Cyrana <damiancyrana@gmail.com>
2021-05-15 09:18:28 +0200

    Added script to Azure folder 'Azure/scripts/creation_azure_policy.ps1'

---
+++ Azure/scripts/creation_azure_policy.ps1
@@ -0,0 +1,13 @@
+﻿# cc
+
+$resourceGroupName = Read-Host -Prompt 'Resource Group Name'
+$NewPolicyAssignmentName = Read-Host -Prompt 'New Policy Assignment Name'
+$NewPolicyAssignmentDisplayName = Read-Host -Prompt 'New Policy Assignment DisplayName'
+
+$resourceGroup = Get-AzResourceGroup -Name '$resourceGroupName'
+
+# get a reference to the built in policy definition to assign
+$policyDefinition = Get-AzPolicyDefinition | Where-Object {$_.Properties.DisplayName -eq 'Audit VMs that do not use managed disks'}
+
+# create the policy assignment by definitionourceID
+New-AzPolicyAssignment -Name $NewPolicyAssignmentName -DisplayName '$NewPolicyAssignmentDisplayName' -Scope $resourceGroupName.ResourceID -PolicyDefinition $policyDefinition
