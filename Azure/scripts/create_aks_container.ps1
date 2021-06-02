c4547789b8d2dbeece3652d5477f061229c7a2be
Damian Cyrana <damiancyrana@gmail.com>
2021-06-02 21:41:16 +0200

    Added Azure/scripts/create_aks_container.ps1

---
+++ Azure/scripts/create_aks_container.ps1
@@ -0,0 +1,16 @@
+﻿#--- Create AKS container ---#
+
+az aks install-cli
+
+$resourceGroup = Read-Host -Prompt 'Enter Resource Group'
+$clusterName = Read-Host -Prompt 'Enter new clouster name'
+$configurationFile = Read-Host -Prompt 'Enter path to YAML file'
+
+#configure kubernetes CLI to use
+az aks Get-Credentials --resource-group $resourceGroup --name $clusterName
+
+#verfiy connection
+kubectl get nodes
+
+#deploy app using the YAML manifest
+kubectl apply -f $configurationFile
